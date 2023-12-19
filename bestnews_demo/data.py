cac6f635c6cf83dd4ad3879452bb18ce79181fff
Natallia Patsiomkina <nataliroundik@gmail.com>
2023-12-19 16:24:23 +0100

    added your_news var and get_posts()

--- bestnews_demo/data.py
+++ bestnews_demo/data.py
@@ -1,8 +1,22 @@
 from datetime import datetime
 
 economic_news = [
-    {"title": "News 1", "summary": "Brief content of the news 1"},
-    {"title": "News 2", "summary": "Краткое содержание новости 2"},
+    {
+        "news_id": 50,
+        "title": "news1",
+        "text": "all content of the news 1 - very long news article",
+        "summary": "short summary of the news 1",  # TODO: text[:140]
+        "created_at": datetime(2027, 12, 31, 10, 30, 00),
+        "author": "David Blaine",
+    },
+    {
+        "news_id": 18,
+        "title": "news2",
+        "text": "all content of the news 1 - very long news article",
+        "summary": "Brief content of the news 2",
+        "created_at": datetime(2027, 12, 31, 10, 30, 00),
+        "author": "David Blaine",
+    }
     # ...
 ]
 it_news = [
@@ -25,11 +39,50 @@ it_news = [
     # ...
 ]
 entertainment_news = [
-    {"title": "News 1", "summary": "Brief content of the news 1"},
-    {"title": "News 2", "summary": "Brief content of the news 2"},
+    {
+        "news_id": 60,
+        "title": "news1",
+        "text": "all content of the news 1 - very long news article",
+        "summary": "short summary of the news 1",  # TODO: text[:140]
+        "created_at": datetime(2027, 12, 31, 10, 30, 00),
+        "author": "David Blaine",
+    },
+    {
+        "news_id": 13,
+        "title": "news2",
+        "text": "all content of the news 1 - very long news article",
+        "summary": "Brief content of the news 2",
+        "created_at": datetime(2027, 12, 31, 10, 30, 00),
+        "author": "David Blaine",
+    }
+    # ...
+]
+
+your_news = [
+    {
+        "news_id": 34,
+        "title": "Updated Title 1",
+        "text": "Updated content of the news 1 - very long news article",
+        "summary": "Updated summary of the news 1",
+        "created_at": datetime(2027, 12, 31, 10, 30, 00),
+        "author": "John Doe",
+    },
+    {
+        "news_id": 44,
+        "title": "Updated Title 2",
+        "text": "Updated content of the news 2 - very long news article",
+        "summary": "Updated content of the news 2",
+        "created_at": datetime(2027, 12, 31, 10, 30, 00),
+        "author": "Jane Doe",
+    }
     # ...
 ]
 
 
 def get_posts():
-    pass
+    return {
+        "it_news": it_news,
+        "economic_news": economic_news,
+        "entertainment_news": entertainment_news,
+        "your_news": your_news,
+    }
