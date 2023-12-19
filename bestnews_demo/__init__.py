c3c2692af8ee6381455c2aea960f3837da97a7e0
gxldstein <gxldstein@tutanota.com>
2023-12-19 06:56:20 +0000

    add basic exception handling for url

--- bestnews_demo/__init__.py
+++ bestnews_demo/__init__.py
@@ -16,14 +16,17 @@ def create_app():
 
     @app.route("/post/<news_id>")
     def post(news_id):
-        news_id = int(news_id)
-        return render_template(
-            "post.html",
-            news_id=news_id,
-            title=data.it_news[news_id]["title"],
-            text=data.it_news[news_id]["text"],
-            author=data.it_news[news_id]["author"],
-            created_at=data.it_news[news_id]["created_at"],
-        )
+        try:
+            news_id = int(news_id)
+            return render_template(
+                "post.html",
+                news_id=news_id,
+                title=data.it_news[news_id]["title"],
+                text=data.it_news[news_id]["text"],
+                author=data.it_news[news_id]["author"],
+                created_at=data.it_news[news_id]["created_at"],
+            )
+        except (IndexError, ValueError):
+            return "404"
 
     return app
