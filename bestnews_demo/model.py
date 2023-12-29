8ca51efec030f3e848fe45d9c016bfee04a57b22
Natallia Patsiomkina <nataliroundik@gmail.com>
2023-12-29 10:42:45 +0100

    corrected created_at, author fields

--- bestnews_demo/model.py
+++ bestnews_demo/model.py
@@ -1,14 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
 class Post(db.Model):
+    __tablename__ = "post"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     text = db.Column(db.String, nullable=True)
-    created_at = db.Column(db.DateTime, nullable=False)
-    author = db.Column(db.String, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    author = db.Column(db.String, nullable=False)
     source = db.Column(db.String, nullable=True)
 
     def __repr__(self):
