1d7df12fc944a9bfadb8ced540bd05cdf4182652
Natallia Patsiomkina <nataliroundik@gmail.com>
2024-01-18 12:14:35 +0100

    added attempts for password

--- bestnews_demo/create_admin.py
+++ bestnews_demo/create_admin.py
@@ -13,13 +13,22 @@ with app.app_context():
         print('User already exists')
         sys.exit(0)
 
-    password = getpass('Enter password: ')
-    password2 = getpass('Repeat password: ')
+    max_attempts = 3
+    for attempt in range(max_attempts):
+        password = getpass('Enter password: ')
+        password2 = getpass('Repeat password: ')
+        if password == password2:
+            break
+        else:
+            print(f'Passwords do not match. Please try again.')
+
     if not password == password2:
+        print(f'Exceeded maximum attempts ({max_attempts})')
         sys.exit(0)
+        
     new_user = User(username=username, is_admin=True)
     new_user.set_password(password)
 
     db.session.add(new_user)
     db.session.commit()
-    print('User with id {} added'.format(new_user.id))
+    print(f'User with id {new_user.id} and username {new_user.username} added')
