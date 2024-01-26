c0299d6b66cb6203ca0375955283930a01148f73
Natallia Patsiomkina <nataliroundik@gmail.com>
2024-01-26 20:55:53 +0100

    adding registration form, corrected templates

--- bestnews_demo/forms.py
+++ bestnews_demo/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, validators
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from .model import User
 
 
 class LoginForm(FlaskForm):
+    username = StringField(
+        "Username",
+        [validators.Length(min=4, max=25)],
+        render_kw={"class": "form-control"},
+    )
+    
+    password = PasswordField(
+        "Password", [validators.DataRequired()], render_kw={"class": "form-control"}
+    )
+    submit = SubmitField("Send", render_kw={"class": "btn btn-primary"})
+
+
+class RegistrationForm(FlaskForm):
     username = StringField(
         "Username",
         [validators.Length(min=4, max=25)],
@@ -17,4 +31,16 @@ class LoginForm(FlaskForm):
     password = PasswordField(
         "Password", [validators.DataRequired()], render_kw={"class": "form-control"}
     )
+    password2 = PasswordField('Repeate Password', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
     submit = SubmitField("Send", render_kw={"class": "btn btn-primary"})
+    def validate_username(self, username):
+        users_count = User.query.filter_by(username=username.data).count()
+        if users_count > 0:
+            raise ValidationError('The user with this name is already registered.')
+
+    def validate_email(self, email):
+        users_count = User.query.filter_by(email=email.data).count()
+        if users_count > 0:
+            raise ValidationError(
+                'The user with this email is already registered.'
+            )
